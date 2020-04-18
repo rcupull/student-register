a473a7eda1f317932030872a3661b195353d26f4
Rodobaldo Cupull <rcupull@gmail.com>
2020-04-18 10:51:40 -0400

    Updates

---
+++ precache-manifest.f62b83f340e2bc55bf091dcd0a92da1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eac455783e00e70a2b097d8f24ee64e",
+    "url": "/student-register/index.html"
+  },
+  {
+    "revision": "1de17e36b27821edf65b",
+    "url": "/student-register/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1de17e36b27821edf65b",
+    "url": "/student-register/static/js/2.644aeba5.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/student-register/static/js/2.644aeba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103325b9b91afaca0029",
+    "url": "/student-register/static/js/main.da483ee6.chunk.js"
+  },
+  {
+    "revision": "0e1d6a7427bd6b8ccde8",
+    "url": "/student-register/static/js/runtime-main.1db47477.js"
+  }
+]);
